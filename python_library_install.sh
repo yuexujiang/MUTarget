ec86609234cf7d867e8e66782dbef0cc8087c482
yuexujiang <jiangyuexugg@gmail.com>
2024-08-06 10:24:04 -0500

    Update python_library_install.sh

--- python_library_install.sh
+++ python_library_install.sh
@@ -6,6 +6,6 @@ conda install conda-forge::pyyaml
 pip install python-box[all]~=7.0 --upgrade
 pip install 'git+https://github.com/katsura-jp/pytorch-cosine-annealing-with-warmup'
 pip install pytest-shutil
-pip install peft
-pip install transformers
+pip install peft==0.11.0
+pip install transformers==4.38.2
 pip install scikit-learn
